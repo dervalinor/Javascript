e87d8d507f96e40adf4c4bb1ae115eecc93d97d7
dervalinor <73512305+dervalinor@users.noreply.github.com>
2021-04-13 15:49:00 -0500

    Add files via upload

--- 2_Operadores/Codigo/codigo.js
+++ 2_Operadores/Codigo/codigo.js
@@ -1,1 +1,36 @@
+//Operadores
 
+/*operador de asignacion: asigna un valor al operando de la izquierda basado en el valor del operando de la derecha, 
+estos operadores son: */ 
+
+//Asignacion x = y 
+//Asignacion de adiccion x += y lo cual es igual a decir x = x + 1 
+let number1 = 4;
+number1 += 3; //para indicar la operacion de number = number + 3
+document.write(number1); //Document.write() es similar a alert() pero imprime el resultado en el
+//body del website en vez de en forma de mensaje de alerta
+
+
+//Asignacion de sustraccion x -= y igual a decir x = x - y
+//Asignacion de multiplicacion x *= y igual a x = x*y
+//Asignacion de division x /= y igual a x = x/y
+//Asignacion de resto x %= y igual a x = x%y
+let number2 = 4, number3 = 3;
+number2 %= number3;
+document.write(number2);
+
+//Asignacion de potenciacion x **= y igual a x = x**y
+//Asignacion de desplazamiento a la izquierda x <<= y igual a x = x << y
+//Asignacion de desplazamiento a la derecha x >>= y igual x = x >> y
+//Asignacion sin signo desplazamiento a la derecha x >>>= y igual x = x >>> y 
+//Asignacion AND x &= y igual a x = x & y
+//Asignacion XOR x ^= y igual a x = x^y 
+//Asignacion OR x |= y igual a x = x | y
+ 
+/*Operadores aritmeticos: los operadores toman valores numericos (ya sean literales o variables) 
+como sus operandos y retornan un valor numerico unico, estos operadores son suma +,resta -,division /,multiplicacion *,potenciacion ** */
+
+let number4 = 5, number5 = 8;
+
+Result = number4/number5;
+document.write("Resultado: " + Result);
--- 2_Operadores/Codigo/codigo3.js
+++ 2_Operadores/Codigo/codigo3.js
@@ -22,7 +22,7 @@ document.write(text1 === text2);
 //mientras si colocamos == nos dice que son iguales
 document.write(text1 ==  text2);
 
-//Non-identify a !== b
+//Non-identify a !== b: para ver si son estrictamente diferentes
 
 //Greater than a > b
 
@@ -31,3 +31,33 @@ document.write(text1 ==  text2);
 //less than a < b
 
 //less than or equal a <= b 
+
+/*Operadores logicos: Nos devuelven un resultado a partir que se cumpla una
+condicion, el resultado es booleano*/
+
+//operador logico AND &&
+//Operador logico OR ||
+//Operador logico NOT ! 
+
+let NUM1 = 3;
+let NUM2 = 9;
+
+let val1 = NUM1 < NUM2;
+let val2 = NUM1 != NUM2;
+
+//true && true = true, false && true = false, es true si la dos condiciones son
+//true
+
+//true || true = true si una de la condiciones es verdadera OR devuelve true 
+//!true = false (Not true = false)
+
+let resultado1 = val1 && val2;//si las dos condiciones se cumplen entonces true
+let resultado2 = val1 || val2;//si una de las dos condiciones se cumplen 
+let resultado3 = !val2;//este operador retorna el valor opuesto 
+
+document.write(" | condicion AND: " + resultado1);
+document.write(" | condicion OR: " + resultado2);
+document.write(" | condicion NOT: " + resultado3);
+
+//camel case: la primera letra con minuscula y la segunda palabra con mayuscula
+//Ejemplo : sumarDosNumeros
