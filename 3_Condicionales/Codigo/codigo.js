502fddc82430a59815ce8e3ecbbef39d0b8c22bd
dervalinor <73512305+dervalinor@users.noreply.github.com>
2021-04-13 16:17:42 -0500

    Add files via upload

--- 3_Condicionales/Codigo/codigo.js
+++ 3_Condicionales/Codigo/codigo.js
@@ -1,16 +1,22 @@
-//condicionales: validar algo
-//if para el caso que se cumpla la condicion
-//else para el caso contrario
+//Problema 1 heladeria
+/*ciclos en Javascript
 
-let validacion;
-
-validacion = prompt("Sind Sie Programmierer: ");
-
-if(validacion == "ja" || validacion == "Ja" || validacion == "JA" || validacion == "jA"){
-  alert("Hallo Programmierer");
-} else if(validacion == "Nein" || validacion == "nein" || validacion == "NEIN"){//else if para en caso de no cumpirse la condicion principal
-  //si cumple otra condicion 
-  alert("Hallo User");
-} else { //else para el caso que no se cumpla ninguna condicion 
-  alert("Opcion no valida");
+while(condicion){
+  
 }
+
+for(var i = 0; i < n; i++){
+
+}
+
+Vectores 
+
+let vector = [];
+
+for(var i = 0; i < 3; i++){
+  vector[i] = prompt("Elemento: ");
+}
+
+for(var i = 0; i < 3; i++){
+  alert(vector[i]);
+}*/
