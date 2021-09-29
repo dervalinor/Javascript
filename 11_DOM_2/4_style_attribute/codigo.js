b1daf6e140404babebdb9077441d113120db940a
dervalinor <dervalar3.14@gmail.com>
2021-09-28 19:38:36 -0500

    Modify all property css with js

---
+++ 11_DOM_2/4_style_attribute/codigo.js
@@ -0,0 +1,6 @@
+//Attribute style: we cam edit all properties of css !!!!
+
+const titulo = document.querySelector(".titulo");
+titulo.style.color = "red"; //modify color
+titulo.style.backgroundColor = "#48e"; //modify background-color
+titulo.style.padding = "30px";
