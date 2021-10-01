2922aadd800682b98a6e48698ba29ce64ba9b611
dervalinor <dervalar3.14@gmail.com>
2021-09-30 20:14:23 -0500

    finished classList

--- 11_DOM_2/5_class/codigo.js
+++ 11_DOM_2/5_class/codigo.js
@@ -17,3 +17,22 @@ titulo.classList.add("bigger");
 //remove
 titulo.classList.add("new");
 titulo.classList.remove("new"); //remove the class called new
+
+//item
+let value  = titulo.classList.item(1);//return name of the second class
+document.write("Name of the second class: " + value);
+
+document.write("<br>");
+
+//contains: verify if exist a class
+value = titulo.classList.contains("bigger"); //verify the class called bigger
+document.write("Verify the class bigger: " + value);
+
+//toggle: case if exist the class then remove and no exist add
+titulo.classList.toggle("grande"); //this case add the class grande
+
+//for no remove class in case if exist use true
+titulo.classList.toggle("bigger", true); //no remove class
+
+//replace
+titulo.classList.replace("grande", "little"); //replace class grande for little
