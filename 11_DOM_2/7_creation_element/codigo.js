89ff28cf3efc4b06cb5c2ce0cd9c6a8aa5112483
dervalinor <dervalar3.14@gmail.com>
2021-10-04 18:47:59 -0500

    finish creation of element html

--- 11_DOM_2/7_creation_element/codigo.js
+++ 11_DOM_2/7_creation_element/codigo.js
@@ -1,6 +1,7 @@
 /*creation of elements
  
 - createElements()
+- createTextNode()
 - appendChild()
 - createDocumentFragment()
 
@@ -11,5 +12,34 @@
 const container = document.querySelector(".container");
 //Now we can create element
 const item = document.createElement("LI");
+
+//if we want li contain inside text then:
+const texto_item = document.createTextNode("This element of the list");
+
 //show type of elememt
-document.write(item);
+console.log(item);
+console.log(texto_item);
+
+//use of child - elements inside of element
+//this case the childs are elements inside of li
+item.appendChild(texto_item);
+
+//other way of modify a element
+//item.innerHTML = "Other element of li";
+
+console.log(item);
+
+//we can add a child a the div
+container.appendChild(item);
+
+//for this case of create much element use createDocumentFragment()
+const fragment = document.createDocumentFragment();
+
+for(let i = 0; i < 20; i++){
+    const item2 = document.createElement("LI");
+    item2.innerHTML = "New element of li";
+    fragment.appendChild(item2);
+}
+
+//show all the elements
+container.appendChild(fragment);
