935ad84c9dc594a415396cf0e4e41daa1152bf6e
dervalinor <dervalar3.14@gmail.com>
2021-10-04 05:54:59 -0500

    creation of elements

---
+++ 11_DOM_2/7_creation_element/codigo.js
@@ -0,0 +1,15 @@
+/*creation of elements
+ 
+- createElements()
+- appendChild()
+- createDocumentFragment()
+
+*/
+
+//we will create a list 
+//first we select element
+const container = document.querySelector(".container");
+//Now we can create element
+const item = document.createElement("LI");
+//show type of elememt
+document.write(item);
