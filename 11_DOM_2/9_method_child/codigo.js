776b21d4d867278b85b37b19475a3345e3bb0094
dervalinor <dervalar3.14@gmail.com>
2021-10-05 18:10:49 -0500

    Start method of child

---
+++ 11_DOM_2/9_method_child/codigo.js
@@ -0,0 +1,14 @@
+/*
+ Method of child
+
+ - replaceChild()
+ - removeChild()
+ - hasChildNodes()
+
+*/
+
+const container = querySelector(".container");
+
+//now we are going to element p and h2
+const parrafo = document.createElement("P").innerHTML = "Simple text";
+const h2 = document.createElement("h2").innerHTML = "Title";
