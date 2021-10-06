5cdf7f9b8b2d164c8d1750bad901d6ce5f056c87
dervalinor <dervalar3.14@gmail.com>
2021-10-06 05:24:11 -0500

    Finished method of child

--- 11_DOM_2/9_method_child/codigo.js
+++ 11_DOM_2/9_method_child/codigo.js
@@ -7,8 +7,30 @@
 
 */
 
-const container = querySelector(".container");
+const container = document.querySelector(".container");
 
 //now we are going to element p and h2
-const parrafo = document.createElement("P").innerHTML = "Simple text";
-const h2 = document.createElement("h2").innerHTML = "Title";
+const p_new = document.createElement("P");
+
+p_new.innerHTML = "Simple text";
+
+const h2_new = document.createElement("h2");
+
+h2_new.innerHTML = "Simple title"; 
+
+h2_old = document.querySelector(".h2");
+
+//now we will replace h2_old for h2_new
+container.replaceChild(h2_new, h2_old);
+
+//Remove a element
+p_old = document.querySelector(".parrafo");
+container.removeChild(p_old);
+
+//verify if have a element son
+let answer = container.hasChildNodes(); //for object use const and variables uselet
+if(answer){ //if answer is true
+    document.write("Element has son"); //a text too is a node and count as son
+ //a text too is a node and count as son} else {
+    document.write("Element has not son");
+}
