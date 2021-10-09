1c5bdea5f8765354084535db28f556fd1d4d215d
dervalinor <dervalar3.14@gmail.com>
2021-10-09 14:58:39 -0500

    first correction of direction of stile.css and icon

---
+++ Problem_javascript/Problem_4/codigo.js
@@ -0,0 +1,16 @@
+/*
+Problema de programacion orientada a Objetos
+
+Cofla entró a una tienda que vende celulares porque le pareció bastante bueno comprarse 
+celular. De todos los modelos que hay disponibles en esa tienda, a Cofla le llamaron la 
+atencion 3 telefonos especificamente. El problema es que ningún vendedor sabe nada al
+respecto entonces no pueden recomendarle absoluto nada ... pero claro: el antes de 
+comprar esos celulares quiere ver las cualidades y comportamientos de cada uno para 
+poder comprarlos:
+
+- crear un sistema para mostrarle a cofla la particularidades de los 3 celulares 
+- cada celular debe tener color, peso, resolucion de pantalla, resolucion de camara 
+y memoria ram
+- cada celular debe poder prender, reiniciar, tomar fotos y grabar
+
+*/
