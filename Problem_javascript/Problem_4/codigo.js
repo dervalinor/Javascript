e487e0eb363793c8e4860ca1d8ed4404b76e0c6d
dervalinor <dervalar3.14@gmail.com>
2021-10-11 21:39:09 -0500

    Problem 4, create objects

--- Problem_javascript/Problem_4/codigo.js
+++ Problem_javascript/Problem_4/codigo.js
@@ -14,3 +14,57 @@ y memoria ram
 - cada celular debe poder prender, reiniciar, tomar fotos y grabar
 
 */
+
+class Phone {
+    //attributes - characterist of phone
+    constructor(mark, color, weight, resolution_screen, resolution_camera, ram){
+        this.mark = mark;
+        this.color = color;
+        this.weight = weight;
+        this.resolution_screen = resolution_screen;
+        this.resolution_camera = resolution_camera;
+        this.ram = ram;
+    }
+
+    //Method of phone
+    static power_on(){
+        console.log(`The phone is powering on`);
+    }
+
+    static power_off(){
+        console.log(`The phone is powering off`);
+    }
+
+    static reboot(){
+        console.log(`The phone is reboot`);
+    }
+    
+    static take_photo(){
+        console.log(`The phone is taking photo`);
+    }
+
+    static take_video(){
+        console.log(`The phone is taking video`);
+    }
+
+    see_info(){
+        console.log(`The phone is mark ${this.mark}, color ${this.color}, 
+        weight ${this.weight}, resolution screen ${this.resolution_screen},
+        resolution camera ${this.resolution_camera} and memory ram ${this.resolution_camera}`);
+    }
+}
+
+//create a class for height gamma where this have slow take video and recognize facial
+//use setters for change attribute of video and security of phone
+class Height_gamma extends Phone {
+    constructor(mark, color, weight, resolution_screen, resolution_screen, ram, speed_video, security_phone){
+        super(mark, color, weight, resolution_screen, resolution_camera, ram);
+        this.speed_video = speed_video;
+        this.security_phone = security_phone;
+    }
+
+    set new_thing(vel_video, sec_phone){
+        this.speed_video = vel_video;
+        this.security_phone = sec_phone;
+    } 
+}
