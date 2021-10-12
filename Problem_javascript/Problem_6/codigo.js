5fd2dc726cb4c51a6853621a423426163e744aef
dervalinor <dervalar3.14@gmail.com>
2021-10-12 08:42:29 -0500

    Resolve problem 6

---
+++ Problem_javascript/Problem_6/codigo.js
@@ -0,0 +1,6 @@
+/*
+
+- Indicale todas la llaves posibles con sus respectiva imagenes y debe seleccionar la indicada
+- una vez seleccionada debe enviar los datos al servidor y que otro programador se encargue
+
+*/ 
